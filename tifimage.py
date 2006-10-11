05f521067e2f6cfe90453211be58257cb04e5996
eknudsen <eknudsen@users.sourceforge.net>
2006-10-11 09:19:36 +0000

    added image stat functions to mimic interface of edfimage class

--- tifimage.py
+++ tifimage.py
@@ -11,6 +11,7 @@ Authors: Henning O. Sorensen & Erik Knudsen
 """
 
 from PIL import Image
+import Numeric
 
 class tifimage:
   data=None
@@ -25,17 +26,48 @@ class tifimage:
       self.read(filename)
       # For some odd reason the getextrema does not work on unsigned 16 bit
       # but it does on 32 bit images, hence convert
-      PILimage = self.data.convert('I')
+      PILimage = self.data#.convert('I')
       return PILimage
     else:
       # For some odd reason the getextrema does not work on unsigned 16 bit
       # but it does on 32 bit images, hence convert
-      PILimage = self.data.convert('I')
+      PILimage = self.data#.convert('I')
       return PILimage
 	
   def read(self,fname,verbose=0):
     print fname
-    self.data=Image.open(fname)
+    self.data=Image.open(fname).convert('I')
     print self.data
     (self.dim1, self.dim2) = self.data.size
+  
+  def write(self):
+    pass
     
+  def getmean(self):
+    if self.m==None:
+      l=list(self.data.getdata())
+      self.m=sum(l)/len(l)
+    return float(self.m)
+  
+  def getmax(self):
+    if self.maxval==None or self.minval==None:
+      self.minval,self.maxval = self.data.getextrema()
+    return self.maxval
+
+  def getmin(self):
+    if self.maxval==None or self.minval==None:
+      self.minval,self.maxval = self.data.getextrema()
+    return self.minval
+  
+  def getstddev(self):
+    if self.stddev==None:
+      self.getmean()
+      N=self.dim1*self.dim2-1
+      l=list(self.data.getdata())
+      l=map(lambda i: (i-self.m)*(i-self.m),l)
+      S=sum(l)/N
+      self.stddev=S
+    return self.stddev
+    
+  def rebin(self,x_rebin_factor, y_rebin_factor):
+    print "rebinning not implemented - yet!"
