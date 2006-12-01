afcafce02643817262045e53e80673249b56b20d
h_osholm <h_osholm@users.sourceforge.net>
2006-12-01 23:25:24 +0000

    now reads all info in peaks.out, and returns info as a dictionary

--- insert_peaks.py
+++ insert_peaks.py
@@ -47,11 +47,38 @@ class readpeaksearch:
                 [npixels, yt, ypos, zpos] =  line.split()[0:4]
                 self.peaks.append([npixels, ypos, zpos])
 
+    def readallpeaks(self,peaksfilename):
+        """
+        read in peaks found with peaksearch (ImageD11)
+        """
+        self.lines = open(peaksfilename,"r").readlines()
+        self.images={}
+        name = 'None'
+        for line in self.lines:
+            if line[0:6] =="# File":
+                if name != 'None': self.images[name]=self.peaks
+                self.peaks = []
+                name = line.split()[-1]
+            elif line[0]!='#' and len(line)>10:
+                    [npixels, yt, ypos, zpos] =  line.split()[0:4]
+                    if npixels > 20:
+                        self.peaks.append([npixels, ypos, zpos])
+                    
+        self.images[name]=self.peaks
+                
+
         
 if __name__=="__main__":
     import sys
     peaksearchfile = sys.argv[1]
-    imagefile = sys.argv[2]
+    try:
+        imagefile = sys.argv[2]
+    except:
+        pass
     peaklist = readpeaksearch()
-    peaklist.readpeaks(peaksearchfile,imagefile)
-    print peaklist.peaks
+    peaklist.readallpeaks(peaksearchfile)
+    pickles = peaklist.images
+
+
+    for k in pickles.keys():
+        print k
