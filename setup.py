d21f44a686d659f37f4c1ab41d60c76268a8e34b
eknudsen <eknudsen@users.sourceforge.net>
2007-06-15 09:53:34 +0000

    1st attempt at the mar345 format

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from distutils.core import setup,Extension
 import sys
+
+mar345_backend=Extension('mar345_io',sources = ['src/mar345_iomodule.c','src/ccp4_pack.c'])
 
 setup(name='fabian',
   version='0.2',
@@ -9,6 +11,7 @@ setup(name='fabian',
   maintainer_email='henning.sorensen@risoe.dk or erik.knudsen@risoe.dk',
   url='http://fable.sourceforge.net',
   py_modules=['edfimage','tifimage','adscimage','brukerimage','bruker100image',
-  'marccdimage','pnmimage','pixel_trace','rocker','image_file_series'],
+  'marccdimage','mar345image','pnmimage','pixel_trace','rocker','image_file_series'],
+  ext_modules=[mar345_backend],
   scripts=["fabian.py"])
 
