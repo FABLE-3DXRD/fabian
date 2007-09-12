a3b78cb36dada2977f5c3d42db1f8f2a982d3910
h_osholm <h_osholm@users.sourceforge.net>
2007-09-12 09:23:48 +0000

    changed hardcoded release no. to be set by make_release

--- scripts/fabian.py
+++ scripts/fabian.py
@@ -1495,7 +1495,7 @@ class About:
         frame.pack()
 
         frameAbout = Frame(frame, bd=0)
-        message = "\nfabian (0.3) was brought to you by \n\n\
+        message = "\nfabian (RELEASE) was brought to you by \n\n\
 Henning O. Sorensen & Erik Knudsen\n\
 Center for Fundamental Research: Metal Structures in Four Dimensions\n\
 Risoe National Laboratory\n\
