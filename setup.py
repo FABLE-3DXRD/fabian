589437b57d4537347975ec28e35db7c404e45b40
h_osholm <h_osholm@users.sourceforge.net>
2009-11-06 09:20:21 +0000

    changed version to 0.7.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 
 setup(
   name='fabian',
-  version='0.6.1',
+  version='0.7.0',
   description='Image viewer for file series of diffraction images',
   license='GPL', maintainer='Henning Osholm Soerensen and Erik Knudsen',
   maintainer_email='henning.sorensen@risoe.dk or erik.knudsen@risoe.dk',
