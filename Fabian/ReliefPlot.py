2c4fce0dd8f1c0d9d57e9813dd9a2b6caa9749e1
Jon Wright <wright@esrf.fr>
2018-02-09 12:26:45 +0100

    color plot

--- Fabian/ReliefPlot.py
+++ Fabian/ReliefPlot.py
@@ -21,6 +21,7 @@ class ReliefPlot:
         self.corners = corners
         data = list(newimage.crop(self.corners).getdata())
         extrema = [min(data),max(data)]
+        self.extrema = extrema
         data = N.reshape( data,[self.corners[3]-self.corners[1], self.corners[2]-self.corners[0]])
         self.map=data.copy()
         self.pointsize=4.
@@ -58,6 +59,7 @@ class ReliefPlot:
         data = list(newimage.crop(self.corners).getdata())
         extrema = [min(data),max(data)]
         data = N.reshape( data,[self.corners[3]-self.corners[1], self.corners[2]-self.corners[0]])
+        extrema = data.min(),data.max()
         self.map = data.copy()
         self.map = N.transpose(self.map)
         h = extrema[1]-extrema[0]
@@ -69,33 +71,9 @@ class ReliefPlot:
         else:
           self.scale = 0.75/self.sizey
         self.zcenter = -(extrema[1]+extrema[0])*self.zscale/2.0
+        self.extrema = extrema
         self.reliefWin.tkRedraw() # Redraw canvas
         
-    def redraw(self,reliefWin):
-         GL.glClearColor(0., 0., 0., 0)
-         GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
-         GL.glOrtho(-1,1,-1,1,-1,1)
-         GL.glDisable(GL.GL_LIGHTING)
-         GL.glColor3f(1.0, 1.0, 1.0) # white
-         GL.glPointSize(self.pointsize)
-         GL.glPushMatrix()
-         GL.glScalef(self.scale,self.scale,self.scale)
-         GL.glRotate(-20,0,0,1)
-         GL.glRotate(-110,1,0,0)
-         GL.glTranslatef(-self.sizex/2.0,-self.sizey/2.0,self.zcenter)
-         for i in range(self.sizex):
-             GL.glBegin(GL.GL_LINE_STRIP)
-             for j in range(self.sizey):
-                 GL.glVertex3f(i,j,self.map[i][j])
-             GL.glEnd()
-         for i in range(self.sizey):
-             GL.glBegin(GL.GL_LINE_STRIP)
-             for j in range(self.sizex):
-                 GL.glVertex3f(j,i,self.map[j][i])
-             GL.glEnd()
-         GL.glEnable(GL.GL_LIGHTING)
-         GL.glPopMatrix()
-
 
     def array_redraw(self,reliefWin):
          # go a bit faster ? 
@@ -110,20 +88,34 @@ class ReliefPlot:
          GL.glRotate(-20,0,0,1)
          GL.glRotate(-110,1,0,0)
          GL.glTranslatef(-self.sizex/2.0,-self.sizey/2.0,self.zcenter)
-         GL.glEnableClientState(GL.GL_VERTEX_ARRAY)
+         GL.glEnableClientState( GL.GL_VERTEX_ARRAY )
+         GL.glEnableClientState( GL.GL_COLOR_ARRAY )
          xyz = N.zeros( (self.sizey,3), N.float32)
          xyz[:,0] = range(self.sizey)
+         c = N.zeros( (self.sizey,3), N.float32)
+         s = self.zscale*(self.extrema[1] - self.extrema[0])
+         l = self.zscale*self.extrema[0]
+         h = self.zscale*self.extrema[1]
          for i in range(self.sizex):
              xyz[:,1] = i
              xyz[:,2] = self.map[i]
+             c[:,0] = (self.map[i]-l)/s
+             c[:,1] = (self.map[i]-l)/s
+             c[:,2] = (h-self.map[i]-l)/s
              GL.glVertexPointer( 3, GL.GL_FLOAT, 0, xyz.tostring() )
+             GL.glColorPointer( 3,  GL.GL_FLOAT, 0, c.tostring() )
              GL.glDrawArrays( GL.GL_LINE_STRIP, 0, self.sizey )
          xyz = N.zeros( (self.sizex, 3), N.float32)
+         c = N.zeros( (self.sizex,3), N.float32)
          xyz[:,1] = range(self.sizex)
          for i in range(self.sizey):
              xyz[:,0] = i
              xyz[:,2] = self.map[:,i]
+             c[:,0] = (self.map[:,i]-l)/s
+             c[:,1] = (self.map[:,i]-l)/s
+             c[:,2] = (h-self.map[:,i]-l)/s
              GL.glVertexPointer( 3, GL.GL_FLOAT, 0, xyz.tostring() )
+             GL.glColorPointer( 3,  GL.GL_FLOAT, 0, c.tostring() )
              GL.glDrawArrays( GL.GL_LINE_STRIP, 0, self.sizex )
          GL.glEnable(GL.GL_LIGHTING)
          GL.glPopMatrix()
